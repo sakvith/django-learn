a74fa70d0186ba4b9f65431d459f6285d3f59852
SAKVITH-X4\Sakvith <devsakvith@gmail.com>
2017-05-06 17:03:17 +0530

    Upto Part 7

--- account/views.py
+++ account/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def home(request):
+
+    numbers = [1,2,3,4,5]
+    name = 'Sakvith'
+
+    args = {'myName':name, 'numbers':numbers}
+
+    return render(request, 'account/login.html', args)
